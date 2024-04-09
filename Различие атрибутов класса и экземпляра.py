20625e9579cec327fb0922a8374a8121139e85b0
Sidvishen <161202115+Sidvishen@users.noreply.github.com>
2024-04-09 12:07:11 +0300

    Различие атрибутов класса и экземпляра.py

--- Различие атрибутов класса и экземпляра.py
+++ Различие атрибутов класса и экземпляра.py
@@ -6,12 +6,9 @@ class Buiding:
         Buiding.total += 1
         print("Buiding.total = ", Buiding.total)
 
-quantity = []
-quantity_size = randint(0, 41)
-while len(quantity) < quantity_size:
-    new_Buiding = Buiding()
-    quantity.append(new_Buiding)
-print(Buiding.total)
+for i in range(40):
+    Buiding.total += 1
+    print("Buiding.total = ", Buiding.total)
 
 
 
