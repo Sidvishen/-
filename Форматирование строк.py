2f1f5a13c9388ee4699dd6c07d6fae9ab231a7df
Sidvishen <161202115+Sidvishen@users.noreply.github.com>
2024-04-17 16:45:41 +0300

    Add files via upload

---
+++ Форматирование строк.py
@@ -0,0 +1,26 @@
+master = 5
+print("В команде Мастера кода участников: %d" % master)
+
+master = 5
+grifindor = 6
+print("Итого сегодня в командах участников: %d и %d" % (master, grifindor))
+
+master = 40
+print("Команда Мастер кода решила задач: {}".format(master))
+
+grifindor = 42
+print("Команда Волшебники данных решила задач: {}".format(grifindor))
+
+team = 18015.2
+print("Волшебники данных решили задачи за {} с".format(team))
+
+master = 40
+grifindor = 42
+print(f"Команды решили {master} и {grifindor} задач")
+
+challenge_result = "победа команды Волшебников данных"
+print(f"Результат битвы: {challenge_result}")
+
+tasks_total = 82
+time_avg = 350.4
+print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу")
