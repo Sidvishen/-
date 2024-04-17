f2a924f9be793e60ae5e4c07fb471baf7bf088ba
Sidvishen <161202115+Sidvishen@users.noreply.github.com>
2024-04-17 12:07:00 +0300

    Режимы открытия файлов.py

--- Режимы открытия файлов.py
+++ Режимы открытия файлов.py
@@ -1,9 +1,8 @@
 from pprint import pprint
 
 faile_name = "Alex.txt"
-faile = open(faile_name, mode="rb")
+faile = open(faile_name, mode="r", encoding="utf-8")
 file_content = faile.read()
 faile.close()
-pprint(file_content)
-
+pprint(file_content.split("\n"))
 
