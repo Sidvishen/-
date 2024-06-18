09c00e2d80a88a669574aaba39f34558f778764f
Sidvishen <161202115+Sidvishen@users.noreply.github.com>
2024-06-18 10:47:40 +0300

    Очереди для обмена данными между потоками.py

--- Очереди для обмена данными между потоками.py
+++ Очереди для обмена данными между потоками.py
@@ -51,7 +51,7 @@ table3 = Table(3)
 tables = [table1, table2, table3]
 cafe = Cafe(tables)
 
-for i in range(1):
-    customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
-    customer_arrival_thread.start()
-    customer_arrival_thread.join()
+
+customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
+customer_arrival_thread.start()
+customer_arrival_thread.join()
