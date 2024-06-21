0baa7cb7ac8fc6f1b7a725c31a17f91279dc024b
Александр Калинин <161202115+Sidvishen@users.noreply.github.com>
2024-06-21 07:49:32 +0300

    Очереди для обмена данными между потоками.py

--- Очереди для обмена данными между потоками.py
+++ Очереди для обмена данными между потоками.py
@@ -1,3 +1,4 @@
+import queue
 import threading
 import time
 
@@ -7,51 +8,67 @@ class Table:
         self.is_busy = False
 class Cafe:
     def __init__(self, tables):
-        self.queue = []
         self.tables = tables
-        self.customer_count = 1
+        self.queue = queue.Queue()
+        self.customers_threads = []
+
+
     def customer_arrival(self):
-        while True:
-            self.serve_customer(self.customer_count)
-            self.customer_count += 1
+        for i in range(1, 21):
+            print(f'Посетитель номер {i} прибыл')
+            self.serve_customer(i)
             time.sleep(1)
-    def serve_customer(self, customer_number):
-        print(f"Посетитель {customer_number} прибыл")
+
+
+    def serve_customer(self, customer):
+        free_table = False
         for table in self.tables:
             if not table.is_busy:
                 table.is_busy = True
-                print(f"Посетитель {customer_number} сел за стол {table.number}")
-                self.start_serving(customer_number, table)
+                print(f'Посетитель номер {customer} сел за стол {table.number}.')
+                customer_thr = Customer(customer, self, self.queue, table)
+                customer_thr.start()
+                self.customers_threads.append(customer_thr)
+                free_table = True
                 return
-        self.queue.append(customer_number)
-        print(f"Посетитель {customer_number} ожидает свободный стол")
-    def start_serving(self, customer_number, table):
-        threading.Thread(target=self.serve, args=(customer_number, table)).start()
-    def serve(self, customer_number, table):
-        time.sleep(5)
-        table.is_busy = False
-        print(f"Посетитель {customer_number} покушал и ушёл")
-
+        if not free_table:
+            print(f'Посетитель номер {customer} ждёт стол.')
+            self.queue.put(customer)
 class Customer(threading.Thread):
-    def __init__(self, cafe, customer_number):
+    def __init__(self, number, cafe, queue, table):
         super().__init__()
         self.cafe = cafe
-        self.customer_number = customer_number
+        self.number = number
+        self.queue = queue
+        self.table = table
+
+
     def run(self):
         time.sleep(5)
-        print(f'Посетитель {self.number} покушал и ушёл')
+        print(f'Посетитель номер {self.number} покушал и ушёл')
         self.table.is_busy = False
         if not self.queue.empty():
             next_customer = self.queue.get()
             self.cafe.serve_customer(next_customer)
 
-table1 = Table(1)
-table2 = Table(2)
-table3 = Table(3)
-tables = [table1, table2, table3]
+
+
+
+tables = []
+for i in range(1, 4):
+    tables.append(Table(i))
+
+
 cafe = Cafe(tables)
 
+th1 = threading.Thread(target=cafe.customer_arrival)
+th1.start()
+th1.join()
 
-customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
-customer_arrival_thread.start()
-customer_arrival_thread.join()
+for i in cafe.customers_threads:
+    i.join()
+
+
+
+
+
