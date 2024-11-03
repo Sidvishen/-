6dad8835ebcaef864b749f56eca75fd92e308ec1
Александр Калинин <161202115+Sidvishen@users.noreply.github.com>
2024-11-03 13:21:55 +0300

    Update Telebot v2.py

--- Telebot v2.py
+++ Telebot v2.py
@@ -1,24 +1,25 @@
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
 import asyncio
 
-api = ('7104415538:AAFJFVuLi5eALVknElHS_KEnp6nq1C9VMWU')
-bot = Bot(token= api)
+api = ('')
+bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-@dp.message_handler(text =['Хэй!'])
+@dp.message_handler(text =['Привет'])
 async def start(message):
-    await message.answer ('Нажмите на команду /start, чтобы начать общение.')
+    await message.answer('Нажмите на команду /start, чтобы начать общение.')
 
 @dp.message_handler(commands=['start'])
 async def start_message(message):
-    await message.answer ('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
 
 @dp.message_handler()
 async def all_message(message):
     print("не назначеные сообщения")
-
-
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
